ba9a5289876dca98cc058570be5349b64a30f759
Gabriel Sicuto <gabrielsicuto@MacBook-Pro-de-Gabriel.local>
2018-10-29 17:58:03 -0300

    creation of maze and objects Hero and Equa, and functions of those

---
+++ src/maze.js
@@ -0,0 +1,179 @@
+// My context
+
+const ctx = document.getElementById('canvas').getContext('2d');
+
+// Key listener function
+
+// Global listeners and variables
+
+const walls = [];
+
+// Constructor Maze
+
+function Maze() {
+  this.sizeX = 100;
+  this.sizeY = 100;
+  this.backGround = new Image();
+  this.backGround.src = 'assets/Img/backGround.png';
+}
+Maze.prototype.draw = function () {
+  const pattern = ctx.createPattern(this.backGround, 'repeat');
+  ctx.fillStyle = pattern;
+  ctx.fillRect(0, 0, 1200, 600);
+};
+Maze.prototype.CreateMaze = function () {
+  const map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
+
+  function drawTile(x, y) {
+    ctx.fillRect(x * 20, y * 20, 20, 20);
+    walls.push({
+      posX: x * 20,
+      posY: y * 20,
+    });
+  }
+
+  ctx.fillStyle = 'black';
+  map.forEach((row, i) => {
+    row.forEach((tile, j) => {
+      if (tile !== 0) {
+        drawTile(j, i);
+      }
+    });
+  });
+};
+
+// Constructor Hero
+
+function Hero(argName) {
+  this.name = argName;
+  this.ligthSize = 60;
+  this.posX = 30;
+  this.posY = 30;
+  this.itens = [];
+  this.equa = false;
+  this.namity = false;
+}
+
+Hero.prototype.Draw = function () {
+  ctx.fillStyle = 'red';
+  ctx.fillRect(this.posX, this.posY, 20, 20);
+};
+
+Hero.prototype.moveRight = function () {
+  this.posX += 10;
+  if (this.DetectColision()) this.posX -= 10;
+};
+Hero.prototype.moveLeft = function () {
+  this.posX -= 10;
+  if (this.DetectColision()) this.posX += 10;
+};
+Hero.prototype.moveUp = function () {
+  this.posY -= 10;
+  if (this.DetectColision()) this.posY += 10;
+};
+Hero.prototype.moveDown = function () {
+  this.posY += 10;
+  if (this.DetectColision()) this.posY -= 10;
+};
+Hero.prototype.move = function (hero, e) {
+  console.log(this);
+  switch (e.keyCode) {
+    case 37:
+      this.moveLeft();
+      break;
+
+    case 39:
+      this.moveRight();
+      break;
+
+    case 38:
+      this.moveUp();
+      break;
+
+    case 40:
+      this.moveDown();
+      break;
+    default:
+  }
+};
+Hero.prototype.DetectColision = function () {
+  let colision = false;
+  for (let i = 0; i < walls.length; i += 1) {
+    if (this.posX < walls[i].posX + 20
+      && this.posX + 20 > walls[i].posX
+      && this.posY < walls[i].posY + 20
+      && 20 + this.posY > walls[i].posY) {
+      // console.log('colision');
+      colision = true;
+    }
+  }
+  console.log(colision);
+  return colision;
+};
+
+// Contructor of Equa
+
+function Equa() {
+  this.posX;
+  this.posY;
+  this.sound;
+}
+
+Equa.prototype.randomPosition = function () {
+  const randomX = Math.floor(Math.random() * (780 - 20 + 1) + 20);
+  const randomY = Math.floor(Math.random() * (580 - 20 + 1) + 20);
+  this.posX = randomX;
+  this.posY = randomY;
+};
+
+Equa.prototype.draw = function () {
+  ctx.fillStyle = 'pink';
+  ctx.fillRect(this.posX, this.posY, 20, 20);
+};
+
+const myMaze = new Maze();
+const myHero = new Hero();
+const myEqua = new Equa();
+myEqua.randomPosition();
+
+function start() {
+  window.addEventListener('keydown', (e) => {
+    myHero.move(myHero, e);
+  }, false);
+  setInterval(engine, 10);
+}
+function engine() {
+  myMaze.draw();
+  myMaze.CreateMaze();
+  myHero.Draw();
+  myEqua.draw();
+}
